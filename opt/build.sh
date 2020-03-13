30020e1b6e554d280f1f26fa89eb6eca5175399a
Nelson Hernandez <nelsonh@gmail.com>
2020-03-13 22:08:09 +0700

    fix zsh-spawned processes' argv[0] substituted with tmux executable path

--- opt/build.sh
+++ opt/build.sh
@@ -8,4 +8,6 @@ export OUTPUT="/opt/releases/tmux-$RELEASE_TAG-x86_64.AppImage"
 /usr/bin/linuxdeploy --appdir=AppDir \
   -i /opt/tmux-logo-square.png \
   -d /opt/tmux.desktop \
-  -e MakeBuild/bin/tmux --output=appimage
+  -e MakeBuild/bin/tmux \
+  --custom-apprun=/opt/AppRun \
+  --output=appimage
