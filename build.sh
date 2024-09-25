ec84656fa8eba503d991257b4d9737bd1f4e1e37
Andrea Olivo <aolivo@psiquantum.com>
2024-09-25 14:44:27 -0700

    build from master

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export TMUX_RELEASE_TAG=3.4
+export TMUX_RELEASE_TAG="master"
 docker build . -t tmux --build-arg TMUX_RELEASE_TAG=$TMUX_RELEASE_TAG
 
 docker rm -f tmuxcontainer
@@ -8,4 +8,4 @@ docker create -ti --name tmuxcontainer tmux bash
 docker cp tmuxcontainer:/opt/build/tmux.appimage .
 docker rm -f tmuxcontainer
 
-zsyncmake -z tmux.appimage
+# zsyncmake -z tmux.appimage
